1747f2ef8da6277bcc733cc010b721a4da8589b8
Matho Camara <matho.camara@gmail.com>
2024-07-31 12:02:29 +0200

    setup asp.net , angular and react projects

---
+++ react-application/Pages/React.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ReactApplication.MyFeature.Pages
+{
+    public class ReactModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
